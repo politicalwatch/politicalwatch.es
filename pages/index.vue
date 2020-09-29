83dd1e1ac621ac5a614f8d7a56c39e7c5e36733d
Daniel Aguilar <dacmail@gmail.com>
2020-09-29 16:56:37 +0200

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <Logo />
+    <nuxt-link
+      v-for="locale in availableLocales"
+      :key="locale.code"
+      :to="switchLocalePath(locale.code)">
+      {{ locale.name }}
+    </nuxt-link>
+
+    <h1 class="title">
+      {{ $t('home') }}
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    availableLocales () {
+      return this.$i18n.locales
+    }
+  }
+}
+</script>
