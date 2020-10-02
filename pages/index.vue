64152f16fef5c0f43cdc14dd5f8733927f088d71
Daniel Aguilar <dacmail@gmail.com>
2020-10-02 13:00:40 +0200

    index page

---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <section>
+    <nuxt-content :document="page" />
+  </section>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content, params, app, error }) {
+    const path = `/${app.i18n.locale}/${params.pathMatch || 'index'}`
+    const [page] = await $content({ deep: true }).where({ path }).fetch()
+    if (!page) {
+      return error({ statusCode: 404, message: 'Page not found' })
+    }
+
+    return { page }
+  },
+  head () {
+    return {
+      title: this.page.title,
+      htmlAttrs: {
+        lang: this.$i18n.locale
+      },
+      meta: [
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: this.page.title
+        },
+        {
+          property: 'og:title',
+          hid: 'og:title',
+          content: this.page.title
+        }
+      ]
+    }
+  }
+}
+</script>
