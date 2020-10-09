c137f2c345c3279b3deb9e7cb4b901bba61c47b3
Daniel Aguilar <dacmail@gmail.com>
2020-10-09 11:01:07 +0200

    footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,13 +1,46 @@
 <template>
   <footer class="c-footer">
     <div class="c-footer__wrapper o-container">
-      Footer
+      <nuxt-link :to="localePath('index')" class="c-footer__logo">
+        <logo />
+      </nuxt-link>
+      <div class="c-footer__address">
+        <p>
+          Avda. Cardenal Herrera Oria <br>
+          63, 3º Izda - 28034, Madrid
+        </p>
+        <p>+34 91 731 48 73</p>
+        <p><a href="mailto:info@politicalwatch.com">info@politicalwatch.com</a></p>
+      </div>
+      <div class="c-footer__social">
+        <a href="#" target="_blank"><facebook /></a>
+        <a href="#" target="_blank"><twitter /></a>
+        <a href="#" target="_blank"><instagram /></a>
+        <a href="#" target="_blank"><youtube /></a>
+      </div>
+    </div>
+    <div class="c-footer__copyright o-container">
+      <p>Politicalwatch © 2020 - <a href="#">Aviso legal</a> - <a href="#">Política de privacidad</a></p>
+      <p>Diseño y desarrollo - <a target="_blank" href="http://ungrynerd.com">UNGRYNERD</a></p>
     </div>
   </footer>
 </template>
 
 <script>
+import logo from '~/assets/images/logo-footer.svg?inline'
+import twitter from '~/assets/images/icon-twitter.svg?inline'
+import facebook from '~/assets/images/icon-facebook.svg?inline'
+import youtube from '~/assets/images/icon-youtube.svg?inline'
+import instagram from '~/assets/images/icon-instagram.svg?inline'
+
 export default {
-  name: 'Footer'
+  name: 'Footer',
+  components: {
+    logo,
+    twitter,
+    facebook,
+    youtube,
+    instagram
+  }
 }
 </script>
