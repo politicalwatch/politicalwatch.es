1bf3e050ef5e19ea8715b8f7d2a0acea6de59129
Daniel Aguilar <dacmail@gmail.com>
2020-10-29 13:23:27 +0100

    FIX: footer html linter

--- components/Footer.vue
+++ components/Footer.vue
@@ -20,7 +20,7 @@
       </div>
     </div>
     <div class="c-footer__copyright o-container">
-      <p>Political Watch © {{ (new Date()).getFullYear() }} - <a href="/aviso-legal">{{ $t('pages.legal.title') }}</a>
+      <p>Political Watch © {{ (new Date()).getFullYear() }} - <a href="/aviso-legal">{{ $t('pages.legal.title') }}</a></p>
       <p>{{ $t('global.developedBy') }} - <a target="_blank" href="http://ungrynerd.com">UNGRYNERD</a></p>
     </div>
   </footer>
