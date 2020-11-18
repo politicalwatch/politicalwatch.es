f8b68aa0a42b0d395a282a241b4253dd9c4f27c6
Daniel Aguilar <dacmail@gmail.com>
2020-11-18 13:34:49 +0100

    force include gtag

--- plugins/googleAnalytics.js
+++ plugins/googleAnalytics.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import VueGtag from 'vue-gtag'
 
 export default ({ app }) => {
-  const getGDPR = localStorage.getItem('GDPR:accepted')
+  // const getGDPR = localStorage.getItem('GDPR:accepted')
 
   Vue.use(VueGtag, {
     config: { id: 'UA-127864699-4' },
-    bootstrap: getGDPR === true,
+    bootstrap: true,
     appName: 'PoliticalWatch',
-    enabled: getGDPR === true,
+    enabled: true,
     pageTrackerScreenviewEnabled: true
   }, app.router)
 }
