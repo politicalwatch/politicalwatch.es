e69677ced8132a2e5cccfb6751913e99f8aedc60
David Hernandez <davidhernandezruiz@gmail.com>
2021-01-12 18:05:29 +0100

    Updates styles for the search page.

--- components/global/Search.vue
+++ components/global/Search.vue
@@ -1,38 +1,41 @@
 <template>
   <div class="c-search">
     <div class="c-search__header">
-      <span class="c-search__button-close" @click="close"><search /></span>
+      <nuxt-link :to="localePath('index')" class="c-search__logo">
+        <logo />
+      </nuxt-link>
+      <span class="c-search__button-close" @click="close"></span>
     </div>
     <div class="c-search__form">
       <input class="c-search__form-input" type="search" placeholder="Buscar..." @change="search" /><button class="c-search__form-button"><iconSearch /></button>
     </div>
-    <div class="c-search__results">
+    <div class="c-search__results c-content">
       <div v-if="tipiResults.length > 0" class="c-search__result">
-        <h2>En Tipi</h2>
+        <h3>En Tipi</h3>
         <ul>
           <li v-for="item in tipiResults" v-bind:key="item">
-            <img src="https://placekitten.com/g/80/80" />
             <p>{{ item }}</p>
           </li>
         </ul>
+         <a href="" class="c-link">Ver más</a>
       </div>
       <div v-if="p2030Results.length > 0" class="c-search__result">
-        <h2>En Parlamento2030</h2>
+        <h3>En Parlamento2030</h3>
         <ul>
           <li v-for="item in p2030Results" v-bind:key="item">
-            <img src="https://placekitten.com/g/80/80" />
             <p>{{ item }}</p>
           </li>
         </ul>
+         <a href="" class="c-link">Ver más</a>
       </div>
       <div v-if="ompResults.length > 0" class="c-search__result">
-        <h2>En OpenManifestoProject</h2>
+        <h3>En OpenManifestoProject</h3>
         <ul>
           <li v-for="item in ompResults" v-bind:key="item">
-            <img src="https://placekitten.com/g/80/80" />
             <p>{{ item }}</p>
           </li>
         </ul>
+         <a href="" class="c-link">Ver más</a>
       </div>
     </div>
   </div>
@@ -40,10 +43,12 @@
 
 <script>
 import iconSearch from '~/assets/images/icon-search.svg?inline'
+import logo from '~/assets/images/logo.svg?inline'
 
 export default {
   components: {
-    iconSearch
+    iconSearch,
+    logo
   },
   data () {
     return {
@@ -57,9 +62,9 @@ export default {
       this.$emit('closed')
     },
     search (event) {
-      this.tipiResults = ['result 1', 'result 2', 'result 3']
-      this.p2030Results = ['result 1', 'result 2', 'result 3']
-      this.ompResults = ['result 1', 'result 2', 'result 3']
+      this.tipiResults = ['Result hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola', 'Result 2', 'Result 3', 'Result 4', 'Result 5']
+      this.p2030Results = ['Result 1', 'Result 2', 'Result 3', 'Result 4', 'Result 5']
+      this.ompResults = ['Result 1', 'Result 2', 'Result 3', 'Result 4', 'Result 5']
     }
   }
 }
