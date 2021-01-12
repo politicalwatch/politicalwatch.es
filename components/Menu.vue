d9c61d3dcc41dd8b222308c06963d9417aa4fdbf
David Hernandez <davidhernandezruiz@gmail.com>
2021-01-12 12:25:50 +0100

    Adds false prototype of the search.

--- components/Menu.vue
+++ components/Menu.vue
@@ -21,6 +21,9 @@
           {{ $t('pages.blog.title') }}
         </nuxt-link>
       </li>
+      <li class="c-menu__item c-menu__item-search">
+        <a href="#" @click="toggleSearch"><iconSearch /></a>
+      </li>
       <!-- <li -->
       <!--   v&#45;for="locale in availableLocales" -->
       <!--   :key="locale.code" -->
@@ -31,22 +34,39 @@
       <!--   </nuxt&#45;link> -->
       <!-- </li> -->
     </ul>
+    <div v-if="searchShown">
+      <search @closed="toggleSearch"/>
+    </div>
   </nav>
 </template>
 
 <script>
+import iconSearch from '~/assets/images/icon-search.svg?inline'
 // import iconLang from '~/assets/images/icon-lang.svg?inline'
 
 export default {
   components: {
     // iconLang
+    iconSearch
   },
   props: {
     active: Boolean
   },
+  data () {
+    return {
+      searchShown: false
+    }
+  },
   computed: {
     availableLocales () {
       return this.$i18n.locales.filter(l => l.code !== this.$i18n.locale)
+    }
+  },
+  methods: {
+    toggleSearch () {
+      if (process.browser) {
+        this.searchShown = !this.searchShown
+      }
     }
   }
 }
---
+++ components/global/Search.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="c-search">
+    <div class="c-search__header">
+      <span class="c-search__button-close" @click="close"><search /></span>
+    </div>
+    <div class="c-search__form">
+      <input class="c-search__form-input" type="search" placeholder="Buscar..." @change="search" /><button class="c-search__form-button"><iconSearch /></button>
+    </div>
+    <div class="c-search__results">
+      <div v-if="tipiResults.length > 0" class="c-search__result">
+        <h2>En Tipi</h2>
+        <ul>
+          <li v-for="item in tipiResults" v-bind:key="item">
+            <img src="https://placekitten.com/g/80/80" />
+            <p>{{ item }}</p>
+          </li>
+        </ul>
+      </div>
+      <div v-if="p2030Results.length > 0" class="c-search__result">
+        <h2>En Parlamento2030</h2>
+        <ul>
+          <li v-for="item in p2030Results" v-bind:key="item">
+            <img src="https://placekitten.com/g/80/80" />
+            <p>{{ item }}</p>
+          </li>
+        </ul>
+      </div>
+      <div v-if="ompResults.length > 0" class="c-search__result">
+        <h2>En OpenManifestoProject</h2>
+        <ul>
+          <li v-for="item in ompResults" v-bind:key="item">
+            <img src="https://placekitten.com/g/80/80" />
+            <p>{{ item }}</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import iconSearch from '~/assets/images/icon-search.svg?inline'
+
+export default {
+  components: {
+    iconSearch
+  },
+  data () {
+    return {
+      tipiResults: [],
+      p2030Results: [],
+      ompResults: []
+    }
+  },
+  methods: {
+    close () {
+      this.$emit('closed')
+    },
+    search (event) {
+      this.tipiResults = ['result 1', 'result 2', 'result 3']
+      this.p2030Results = ['result 1', 'result 2', 'result 3']
+      this.ompResults = ['result 1', 'result 2', 'result 3']
+    }
+  }
+}
+</script>
