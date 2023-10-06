9de2dd4f56d4785094322eb464a484f517314fab
Alex Ahumada <projectsperminute@gmail.com>
2023-10-06 14:44:45 +0200

    Adds GA4 tag

--- plugins/googleAnalytics.js
+++ plugins/googleAnalytics.js
@@ -5,7 +5,7 @@ export default ({ app }) => {
   const getGDPR = localStorage.getItem('GDPR:accepted')
 
   Vue.use(VueGtag, {
-    config: { id: 'UA-127864699-4' },
+    config: { id: 'G-15GZ6731CG' },
     bootstrap: getGDPR,
     appName: 'PoliticalWatch',
     enabled: getGDPR,
