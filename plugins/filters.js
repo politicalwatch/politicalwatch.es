5f84bf99e2d807be360ac0d59c97d5b46f35022d
Alex Ahumada <projectsperminute@gmail.com>
2024-05-02 16:05:39 +0200

    Removes unused mixins, plugins and filters

--- plugins/filters.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-
-Vue.filter('urldecode', val => val.replace(/-/g, ' '))
-Vue.filter('formatDate', (date, locale = 'es', options = { year: 'numeric', month: 'long', day: 'numeric' }) => new Date(date).toLocaleDateString(locale, options))
