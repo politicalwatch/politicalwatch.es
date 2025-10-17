c7c93bb714120c7712329de80542d05c630794f9
Alex Ahumada <alex.ahumada@politicalwatch.es>
2025-10-17 10:56:24 +0200

    fix: restore MdContent and update to nuxt-content v3

---
+++ components/global/MdContent.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="c-content o-section">
+    <div class="c-content__wrapper">
+      <slot />
+    </div>
+    <nav v-if="withProjectsToc" class="c-projects-toc">
+      <div class="c-projects-toc__wrapper">
+        <h3 class="c-projects-toc__title">
+          {{ t("blocks.projects.title") }}
+        </h3>
+        <ul class="c-projects-toc__list">
+          <li
+            v-for="(project, i) in projects"
+            :key="i"
+            class="c-projects-toc__item"
+          >
+            <a
+              :href="project.link"
+              target="_blank"
+              class="c-projects-toc__link"
+              >{{ project.title }}</a
+            >
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </section>
+</template>
+
+<script setup lang="ts">
+const { withProjectsToc, projectLimit } = defineProps({
+  withProjectsToc: {
+    type: Boolean,
+    default: false,
+  },
+  projectLimit: {
+    type: Number,
+    default: null,
+  },
+});
+
+const { t, locale } = useI18n();
+
+const { data: projects } = await useAsyncData("proyectos", () => {
+  let query = queryCollection("proyectos")
+    .where("locale", "=", locale.value)
+    .order("order", "DESC");
+
+  if (projectLimit) query = query.limit(projectLimit);
+
+  return query.all();
+});
+</script>
