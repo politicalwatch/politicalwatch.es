7818f5e72700c5415f17d0cd367d572e30cdeba0
Alex Ahumada <alex.ahumada@politicalwatch.es>
2026-05-26 09:31:57 +0200

    feat: move Derechos Digitales Banner to its own component

---
+++ app/components/global/derechos-digitales/Banner.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="c-dd-banner">
+    <div class="c-dd-banner__wrapper o-container">
+      <p class="c-dd-banner__text">
+        {{ t('pages.derechosDigitales.bannerText1') }}<br />
+        {{ t('pages.derechosDigitales.bannerText2') }}
+      </p>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+const { t } = useI18n();
+</script>
+
+<style lang="scss" scoped>
+.c-dd-banner {
+  background: linear-gradient(
+    to right,
+    $dd-gradient-from 0%,
+    $dd-gradient-via 33%,
+    $dd-gradient-via-2 66%,
+    $dd-gradient-to 100%
+  );
+  padding: gap(5) 0;
+
+  @media (min-width: $md) {
+    padding: gap(6) 0;
+  }
+
+  &__wrapper {
+    text-align: center;
+  }
+
+  &__text {
+    font-family: $font-primary;
+    font-size: rem(18px);
+    font-weight: 500;
+    line-height: 1.6;
+    color: $white;
+    margin: 0;
+    margin-inline: auto;
+
+    @media (min-width: $md) {
+      font-size: rem(24px);
+    }
+  }
+}
+</style>
--- app/pages/derechos-digitales/index.vue
+++ app/pages/derechos-digitales/index.vue
@@ -2,14 +2,7 @@
   <main class="c-page is-dd">
     <DerechosDigitalesHero />
     <DerechosDigitalesIntro />
-    <section class="c-dd-banner">
-      <div class="c-dd-banner__wrapper o-container">
-        <p class="c-dd-banner__text">
-          {{ t('pages.derechosDigitales.bannerText1') }}<br />
-          {{ t('pages.derechosDigitales.bannerText2') }}
-        </p>
-      </div>
-    </section>
+    <DerechosDigitalesBanner />
 
   </main>
 </template>
@@ -43,39 +36,3 @@ main.is-dd + .c-newsletter {
 }
 </style>
 
-<style lang="scss" scoped>
-.c-dd-banner {
-  background: linear-gradient(
-    to right,
-    $dd-gradient-from 0%,
-    $dd-gradient-via 33%,
-    $dd-gradient-via-2 66%,
-    $dd-gradient-to 100%
-  );
-  padding: gap(5) 0;
-  margin-top: gap(8);
-
-  @media (min-width: $md) {
-    padding: gap(6) 0;
-    margin-top: gap(12);
-  }
-
-  &__wrapper {
-    text-align: center;
-  }
-
-  &__text {
-    font-family: $font-primary;
-    font-size: rem(18px);
-    font-weight: 500;
-    line-height: 1.6;
-    color: $white;
-    margin: 0;
-    margin-inline: auto;
-
-    @media (min-width: $md) {
-      font-size: rem(24px);
-    }
-  }
-}
-</style>
